49d5916be182a18ef18326a7f9a049c89347ba1f
Luis Silva <silva@broadinstitute.org>
2025-02-28 12:29:39 -0500

    feat: adding support for rhel9

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 # Installs and configures rssh.
-class rssh(
+class rssh (
   $package     = 'rssh',
   $config_file = '/etc/rssh.conf',
   $config_mode = '0644',
@@ -9,13 +9,26 @@ class rssh(
   $chrootpath  = false,
   $users       = []
 ) {
-  package { $package:
-    ensure => present,
-    before => File[$config_file],
+  if $facts['os']['release']['major'] > '8' {
+    exec { 'rssh-download':
+      command => 'wget http://prdownloads.sourceforge.net/rssh/rssh-2.1.1-1.RH9.i386.rpm?download -O /tmp/rssh-2.1.1-1.RH9.i386.rpm',
+      path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+      unless  => 'rpm -q rssh',
+    }
+    exec { 'rssh-install':
+      command => 'rpm -i /tmp/rssh-2.1.1-1.RH9.i386.rpm',
+      path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+      require => Exec['rssh-download'],
+      unless  => 'rpm -q rssh',
+    }
+  } else {
+    package { $package:
+      ensure => present,
+      before => File[$config_file],
+    }
   }
-
   file { $config_file:
-    ensure  => present,
+    ensure  => file,
     mode    => $config_mode,
     content => template('rssh/rssh.conf.erb'),
   }
